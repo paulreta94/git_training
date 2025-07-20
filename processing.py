e205933d070640d0c9db3d216e85dfa2541c84e5
peret <paul-etienne.retaux@ensea.fr>
2025-07-20 18:17:15 +0200

    issue

--- processing.py
+++ processing.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import numpy as np
-df = pd.DataFrame({"column 1": np.linspace(0,3,4), "column 2" : np.array([3, 4, np.nan, np.nan])})
+df = pd.DataFrame({"column 1": np.linspace(0,3,5), "column 2" : np.array([3, 4, np.nan, np.nan, np.nan])})
 df.to_json("written.json")
